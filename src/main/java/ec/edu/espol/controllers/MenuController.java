d3b35a2b231abda9821dbeeade5a4dcfc613d61a
Frankcux <anlosanc@espol.edu.ec>
2022-01-26 01:19:47 -0500

    Se agrego menu.fxml

---
+++ src/main/java/ec/edu/espol/controllers/MenuController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author angel
+ */
+public class MenuController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void concurso(MouseEvent event) {
+    }
+
+    @FXML
+    private void mascota(MouseEvent event) {
+    }
+
+    @FXML
+    private void dueno(MouseEvent event) {
+    }
+
+    @FXML
+    private void premio(MouseEvent event) {
+    }
+
+    @FXML
+    private void salir(MouseEvent event) {
+    }
+
+    @FXML
+    private void criterio(MouseEvent event) {
+    }
+
+    @FXML
+    private void inscripcion(MouseEvent event) {
+    }
+
+    @FXML
+    private void MiembroJurado(MouseEvent event) {
+    }
+
+    @FXML
+    private void evaluacion(MouseEvent event) {
+    }
+    
+}
