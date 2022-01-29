34f1f2824498e16f3683e576fdc6bee67ebe3d7d
adairaxe <aabrigo@espol.edu.ec>
2022-01-29 01:45:28 -0500

    Cambios en el controlador de Inscripcion

--- src/main/java/ec/edu/espol/controllers/InscripcionController.java
+++ src/main/java/ec/edu/espol/controllers/InscripcionController.java
@@ -11,8 +11,12 @@ import ec.edu.espol.modelfmxl.Util;
 import ec.edu.espol.proyectosegundopar.App;
 import java.io.IOException;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -53,22 +57,48 @@ public class InscripcionController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
 
+    
+    public static Date ParseFecha(String fecha)
+    {
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        Date fechaDate = null;
+        try {
+            fechaDate = formato.parse(fecha);
+        } 
+        catch (ParseException ex) 
+        {
+            System.out.println(ex);
+        }
+        return fechaDate;
+    }
+
+
+
+    
     @FXML
     private void guardarInscripcion(MouseEvent event) {
         ArrayList<Inscripcion> lista_inscripciones = Inscripcion.readFile("inscripcion.txt");
         int id_inscripcion = lista_inscripciones.size()+1;
         
         int id_mascota = Util.examinarIdMascota(txtNombreMascota.getText());
+
+        if (id_mascota == 0){
+            Alert alertaMascota = new Alert(AlertType.ERROR, "Revise si el nombre de su mascota es correcto");
+            alertaMascota.show();
+        }
+                
+        
         int id_concurso = Util.examinarIdConcurso(txtNombreConcurso.getText());
-        LocalDate fecha_actual = LocalDate.now();
-        double precio_concurso = Double.parseDouble(txtTotalPagar.getText());
+        if (id_mascota == 0){
+            Alert alertaConcurso = new Alert(AlertType.ERROR, "Revise si el nombre del concurso es correcto");
+            alertaConcurso.show();
+        }
         
-        Alert alertfecha;
-        Alert alertacosto;
-        
-        
+        Alert alertfecha;         
+        LocalDate fecha_actual = LocalDate.parse(txtFechaInscripcion.getText());        
         if (fecha_actual.isBefore(Util.fecha_inicio_concurso(id_concurso))){    
             alertfecha = new Alert(AlertType.ERROR,"LA FECHA DE INSCRIPCION AUN NO COMIENZA");          
         }       
@@ -76,23 +106,22 @@ public class InscripcionController implements Initializable {
             alertfecha = new Alert(AlertType.ERROR,"LA FECHA DE INSCRIPCION HA TERMINADO");
         }
         
-        
+        Alert alertacosto;
+        double precio_concurso = Double.parseDouble(txtTotalPagar.getText());
         if (precio_concurso > Util.exminarCostoConcurso(id_concurso)){
             alertacosto = new Alert(AlertType.ERROR,"Usted está pagando de más"); 
         }
         else if(precio_concurso < Util.exminarCostoConcurso(id_concurso)){
-            alertacosto = new Alert(AlertType.ERROR,"El costo a pagar es de\n"+Util.exminarCostoConcurso(id_concurso));           
+            alertacosto = new Alert(AlertType.ERROR,"El costo a pagar es de\n");           
         }
         
         //Inscripcion(int id, LocalDate fecha_inscripcion, double valor, int idMascota,int idConcurso )
         Inscripcion inscripcion = new Inscripcion(id_inscripcion, fecha_actual, precio_concurso, id_mascota, id_concurso);
         inscripcion.saveFile("inscripcion.txt");
-        
-        
-        
-        
-        
+
     }
+    
+    
 
     @FXML
     private void regresar(MouseEvent event) throws IOException {
